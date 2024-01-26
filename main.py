5136dffecbe9f84768b941001b07928247e967be
qiscelc <69080553+qiscelc@users.noreply.github.com>
2024-01-26 15:21:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ app = Flask(__name__)
 def index():
     return "index page"
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 8000))
+    app.run(host=host, port=port, debug=True)
