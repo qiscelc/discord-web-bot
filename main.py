6e800d8baac88214413dc18c55b94c1bb42a2f7f
qiscelc <69080553+qiscelc@users.noreply.github.com>
2024-01-26 15:25:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,4 @@ def index():
     return "index page"
 
 if __name__ == "__main__":
-    host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 8000))
-    app.run(host=host, port=port, debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8000)
